e64a7887282d0ad3802397c043922a304255a962
Lukas Hartinger <lukas.hartinger@gmail.com>
2024-03-03 16:15:38 +0100

    Do some setup

---
+++ Project/Character/Movement.cs
@@ -0,0 +1,56 @@
+using Godot;
+using System;
+
+public partial class Movement : CharacterBody2D
+{
+	[Export]
+	public int speed;
+
+	private Vector2 _targetVelocity = Vector2.Zero;
+
+	Node2D pivot;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		pivot = GetNode<Node2D>("Pivot");
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		var direction = Vector2.Zero;
+
+		if (Input.IsActionPressed("move_left"))
+		{
+			direction.X -= 1.0f;
+		}
+
+		if (Input.IsActionPressed("move_right"))
+		{
+			direction.X += 1.0f;
+		}
+
+		if (Input.IsActionPressed("move_up"))
+		{
+			direction.Y -= 1.0f;
+		}
+
+		if (Input.IsActionPressed("move_down"))
+		{
+			direction.Y += 1.0f;
+		}
+
+		if (direction != Vector2.Zero)
+		{
+			direction = direction.Normalized();
+
+		}
+
+		_targetVelocity.X = direction.X * speed;
+		_targetVelocity.Y = direction.Y * speed;
+
+		Velocity = _targetVelocity;
+
+		MoveAndSlide();
+	}
+}
