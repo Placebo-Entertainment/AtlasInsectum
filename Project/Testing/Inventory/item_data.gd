fc26900a17bf0238ce496ff053fcb1e847f938d1
Michiocre <michi2806@gmail.com>
2024-03-03 16:21:48 +0100

    Added testing inventory stuff

---
+++ Project/Testing/Inventory/item_data.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name ItemData
+
+@export var name: String = ""
+@export_multiline var description: String = ""
+@export var stack_size: int = 99
+@export var texture: AtlasTexture
